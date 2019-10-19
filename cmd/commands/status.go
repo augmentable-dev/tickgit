02224a05506917798ce0dc1fe1f3503b9f552084
Patrick DeVivo <patrick.devivo@gmail.com>
2019-10-18 22:43:38 -0400

    let the caller supply a local git repo

--- cmd/commands/status.go
+++ cmd/commands/status.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/augmentable-dev/tickgit"
 	"github.com/spf13/cobra"
@@ -22,9 +23,16 @@ var versionCmd = &cobra.Command{
 	Use:   "status",
 	Short: "Print a status report of the current directory",
 	Long:  ``,
+	Args:  cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		dir, err := os.Getwd()
+		cwd, err := os.Getwd()
 		handleError(err)
+
+		dir := cwd
+		if len(args) == 1 {
+			dir, err = filepath.Rel(cwd, args[0])
+			handleError(err)
+		}
 
 		r, err := git.PlainOpen(dir)
 		handleError(err)
