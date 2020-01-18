07be0a8fa9d2bcc2f5d2369c7be207126e9b4725
Patrick DeVivo <patrick.devivo@gmail.com>
2020-01-18 13:26:17 -0500

    first step of removing extraneous functionality

--- cmd/commands/root.go
+++ cmd/commands/root.go
@@ -5,14 +5,7 @@ import (
 	"os"
 
 	"github.com/briandowns/spinner"
-	"github.com/spf13/cobra"
 )
-
-var rootCmd = &cobra.Command{
-	Use:   "tickgit",
-	Short: "Tickets as config",
-	Long:  `tickgit is a tool for helping you manage tickets and todos in your codebase, as a part of your git history`,
-}
 
 // TODO clean this up
 func handleError(err error, spinner *spinner.Spinner) {
@@ -30,7 +23,7 @@ func handleError(err error, spinner *spinner.Spinner) {
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := todosCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
