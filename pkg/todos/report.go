7040cf43c4498ae256893a8205f75f4c485b6280
Patrick DeVivo <patrick.devivo@gmail.com>
2020-03-11 18:51:24 -0400

    first pass at some work to support more match phrases
    not yet configurable, but adds matching for "TODO", "FIXME", "OPTIMIZE", "HACK", "XXX", "WTF", "LEGACY" and their @ + lowerCase version equivalents

--- pkg/todos/report.go
+++ pkg/todos/report.go
@@ -2,13 +2,13 @@ package todos
 
 import (
 	"io"
+	"strings"
 	"text/template"
 )
 
-// DefaultTemplate is the default report template
-const DefaultTemplate = `
+const defaultTemplate = `
 {{- range $index, $todo := . }}
-{{ print "\u001b[33m" }}TODO{{ print "\u001b[0m" }}: {{ .String }}
+{{ .String }}
   => {{ .Comment.FilePath }}:{{ .Comment.StartLocation.Line }}:{{ .Comment.StartLocation.Pos }}
   {{- if .Blame }}
   => added {{ .TimeAgo }} by {{ .Blame.Author }} in {{ .Blame.SHA }}
@@ -22,9 +22,15 @@ no todos 🎉
 // WriteTodos renders a report of todos
 func WriteTodos(todos ToDos, writer io.Writer) error {
 
-	t, err := template.New("todos").Parse(DefaultTemplate)
+	t, err := template.New("todos").Parse(defaultTemplate)
 	if err != nil {
 		return err
+	}
+
+	// replace the phrase in the todo string with a "highlighted" version for console output
+	// TODO eventually make this configurable, for NO_COLOR output (or customization of color?)
+	for _, todo := range todos {
+		todo.String = strings.Replace(todo.String, todo.Phrase, "\u001b[33m"+todo.Phrase+"\u001b[0m", 1)
 	}
 
 	err = t.Execute(writer, todos)
