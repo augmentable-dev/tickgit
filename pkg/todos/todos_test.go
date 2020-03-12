184d42a751a54f79172af7dc5c949ca5f927e936
Patrick DeVivo <patrick.devivo@gmail.com>
2020-03-11 21:19:46 -0400

    while we're here, add some simple test cases

---
+++ pkg/todos/todos_test.go
@@ -0,0 +1,36 @@
+package todos
+
+import (
+	"testing"
+
+	"github.com/augmentable-dev/lege"
+	"github.com/augmentable-dev/tickgit/pkg/comments"
+)
+
+func TestNewToDoNil(t *testing.T) {
+	collection := lege.NewCollection(lege.Location{}, lege.Location{}, lege.Boundary{}, "Hello World")
+	comment := comments.Comment{
+		Collection: *collection,
+	}
+	todo := NewToDo(comment)
+
+	if todo != nil {
+		t.Fatalf("did not expect a TODO, got: %v", todo)
+	}
+}
+
+func TestNewToDo(t *testing.T) {
+	collection := lege.NewCollection(lege.Location{}, lege.Location{}, lege.Boundary{}, "TODO Hello World")
+	comment := comments.Comment{
+		Collection: *collection,
+	}
+	todo := NewToDo(comment)
+
+	if todo == nil {
+		t.Fatalf("expected a TODO, got: %v", todo)
+	}
+
+	if todo.Phrase != "TODO" {
+		t.Fatalf("expected matched phrase to be TODO, got: %s", todo.Phrase)
+	}
+}
